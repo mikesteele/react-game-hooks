08ecd1118e9f6515d81ec8703a0ec956169d41b0
Mike Steele <mike@steel.fm>
2020-04-08 22:09:19 -0400

    List all demos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import PositionDemo from './demos/PositionDemo';
 import PongDemo from './demos/PongDemo';
 import ChromeDinosaurDemo from './demos/ChromeDinosaurDemo';
-import SpaceInvaderDemo from './demos/SpaceInvaderDemo';
+import { css } from 'emotion';
+
+const button = css`
+  font-size: 24px;
+  padding: 8px;
+  margin: 8px;
+`;
+
+const demos = {
+  position: <PositionDemo />,
+  pong: <PongDemo />,
+  dinosaur: <ChromeDinosaurDemo />
+}
 
 const App = () => {
-  return (
-    <SpaceInvaderDemo />
-  )
+  const [demo, setDemo] = React.useState('');
+  if (!demo) {
+    return Object.keys(demos).map(name => (
+      <button className={button} onClick={() => setDemo(name)}>
+        {name}
+      </button>
+    ));
+  } else {
+    return demos[demo];
+  }
 };
 
 export default App;
